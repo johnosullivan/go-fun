4ae23ce10179dfee28ae814696334fc152d46a38
John O'Sullivan <jnosullivan@icloud.com>
2020-04-21 19:23:45 -0400

    Update main.go for Jenkins test

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ type User struct {
 
 func pingLink(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	user := User{Name: "John Doe Test", Email: "johndoe@gmail.com"}
+	user := User{Name: "John (Jenkins) Test", Email: "johndoe@gmail.com"}
 	json.NewEncoder(w).Encode(user)
 }
 

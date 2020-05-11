c58efc9437af5ccaba264f7684aa2e6aef481596
John O'Sullivan <jnosullivan@icloud.com>
2020-05-10 22:05:00 -0400

    Clean up

--- controllers/system.go
+++ controllers/system.go
@@ -3,13 +3,24 @@ package controllers
 import (
   "io"
 	"net/http"
+  "time"
+  "encoding/json"
 )
 
+type SystemStatus struct {
+  Date    time.Time
+  Status  bool
+}
+
 func PingLink(w http.ResponseWriter, r *http.Request) {
-  	w.Header().Set("Content-Type", "application/json")
-  	//res := utilities.Response{Key: "", Secret: ""}
-  	//json.NewEncoder(w).Encode(res)
-    io.WriteString(w, `{"status":"ok"}`)
+    sysStatus := SystemStatus{time.Now(), true}
+    js, err := json.Marshal(sysStatus)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(js)
 }
 
 func AuthPingHandler(w http.ResponseWriter, r *http.Request) {
--- main.go
+++ main.go
@@ -17,25 +17,43 @@ import (
 	//"github.com/johnosullivan/go-fun/db"
 )
 
+type loggingResponseWriter struct {
+    http.ResponseWriter
+    statusCode int
+}
+
+func LoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+    return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+    lrw.statusCode = code
+    lrw.ResponseWriter.WriteHeader(code)
+}
+
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.WithFields(log.Fields{
-	       "remote_addr": r.RemoteAddr,
-	       "method": r.Method,
-				 "url": r.URL,
-				 "userAgent": r.Header.Get("User-Agent")}).Info("request")
+		lrw := LoggingResponseWriter(w)
 
-		handler.ServeHTTP(w, r)
+		handler.ServeHTTP(lrw, r)
+
+		log.WithFields(log.Fields{"remote_addr": r.RemoteAddr,
+	       "method": r.Method,
+				 "url": r.URL.String(),
+				 "userAgent": r.Header.Get("User-Agent"),
+				 "statusCode": lrw.statusCode}).Info("")
 	})
 }
 
 func main() {
-	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	/*file, err := os.OpenFile(os.Getenv("LOG_PATH"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
   if err != nil {
       log.Fatal(err)
   }
   defer file.Close()
 	log.SetOutput(file)
+	log.SetFormatter(&log.JSONFormatter{})*/
+
 	log.SetFormatter(&log.JSONFormatter{})
 
 	var wait time.Duration

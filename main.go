a5eaa6ed531e4eea5ea0d9dcd4aab79c4f511f32
John O'Sullivan <jnosullivan@icloud.com>
2020-05-13 22:29:27 -0400

    Updated packake

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 	"flag"
 	"os/signal"
+	"strconv"
 
 	log "github.com/sirupsen/logrus"
 
@@ -16,41 +17,19 @@ import (
 	//"github.com/johnosullivan/go-fun/db"
 )
 
-type loggingResponseWriter struct {
-    http.ResponseWriter
-    statusCode int
-}
-
-func LoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
-    return &loggingResponseWriter{w, http.StatusOK}
-}
-
-func (lrw *loggingResponseWriter) WriteHeader(code int) {
-    lrw.statusCode = code
-    lrw.ResponseWriter.WriteHeader(code)
-}
-
-func logRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		lrw := LoggingResponseWriter(w)
-
-		handler.ServeHTTP(lrw, r)
-
-		log.WithFields(log.Fields{"remote_addr": r.RemoteAddr,
-	       "method": r.Method,
-				 "url": r.URL.String(),
-				 "userAgent": r.Header.Get("User-Agent"),
-				 "statusCode": lrw.statusCode}).Info("")
-	})
-}
 
 func main() {
-	file, err := os.OpenFile(os.Getenv("LOG_PATH"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-  if err != nil {
-      log.Fatal(err)
-  }
-  defer file.Close()
-	log.SetOutput(file)
+	logfile, err := strconv.ParseBool(os.Getenv("LOGFILE"))
+  utilities.CheckError(err)
+
+	if logfile {
+		file, err := os.OpenFile(os.Getenv("LOG_PATH"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	  if err != nil {
+	      log.Fatal(err)
+	  }
+	  defer file.Close()
+		log.SetOutput(file)
+	}
 	log.SetFormatter(&log.JSONFormatter{})
 
 	var wait time.Duration
@@ -73,7 +52,7 @@ func main() {
       WriteTimeout: time.Second * 15,
       ReadTimeout:  time.Second * 15,
       IdleTimeout:  time.Second * 60,
-      Handler: logRequest(router),
+      Handler: utilities.LogRequest(router),
   }
 
   go func() {
